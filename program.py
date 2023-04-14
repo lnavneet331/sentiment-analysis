b6cf771651659e6787060ab8956a405d26eb6d5e
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-15 00:08:21 +0530

    Committing at 2023-04-14 18:38:20.804718

--- program.py
+++ program.py
@@ -369,3 +369,13 @@ def remove_digits1(sample_text):
 
 df_new['Review'] = df_new.Review.apply(lambda x: remove_digits1(x))
 
+
+# In[48]:
+
+
+def remove_emojis(data):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
